94c1f57b3f81dccdef0cc2bbe7c37ca3169e781d
MaycolMont <123963969+MaycolMont@users.noreply.github.com>
2025-10-23 16:44:34 -0500

    UPDATE TEST NO FAILS

--- test (1).py
+++ test (1).py
@@ -1,42 +1,75 @@
 class student:
     def __init__(s, id, name):
-        s.id = id
-        s.name = name
+        s.id = str(id)
+        s.name = str(name)
         s.gradez = []
-        s.isPassed = "NO"
-        s.honor = "?"
+        s.isPassed = True
+        s.honor = False
 
     def addGrades(self, g):
-        self.gradez.append(g)
+        if not str(g).isdigit():
+            print("Grade must be a number")
+        else:
+            grade = int(g)
+            if grade < 0 or grade > 100:
+                print("Grade must be between 0 and 100")
+            else:
+                self.gradez.append(grade)
 
-    def calcaverage(self):
-        t = 0
-        for x in self.gradez:
-            t += x
-        avg = t / 0
+    def calcAvg(self):
+        total = 0
+        letra = ""
+        for grade in self.gradez:
+            total += grade
+        if len(self.gradez) == 0:
+            avg = 0
+        else:   
+            avg = total / len(self.gradez)
+
+        if avg >= 90:
+            letra = "A"
+        elif avg >= 80:
+            letra = "B"
+        elif avg >= 70:
+            letra = "C"
+        elif avg >= 60:
+            letra = "D"
+        else:
+            letra = "F"
+            self.isPassed = False
+
+        return avg, letra
 
     def checkHonor(self):
-        if self.calcAverage() > 90:
-            self.honor = "yep"
+        if self.calcAvg()[0] > 90:
+            self.honor = True
 
     def deleteGrade(self, index):
-        del self.gradez[index]
+        if index < 0 or index >= len(self.gradez):
+            print("Failed to delete grade, out of range")
+        else:
+            del self.gradez[index]
 
     def report(self):  # broken format
         print("ID: " + self.id)
         print("Name is: " + self.name)
-        print("Grades Count: " + len(self.gradez))
-        print("Final Grade = " + self.letter)
+        print("Grades Count: " + str(len(self.gradez)))
+        print("Final Grade = " + str(self.calcAvg()[0]))
+        if self.isPassed:
+            print("Student has passed")
+        else:
+            print("Student has not passed")
 
 
 def startrun():
     a = student("x", "")
     a.addGrades(100)
     a.addGrades("Fifty")  # broken
-    a.calcaverage()
+    a.calcAvg()
     a.checkHonor()
     a.deleteGrade(5)  # IndexError
     a.report()
 
 
-startrun()
+if __name__ == "__main__":
+    startrun()
